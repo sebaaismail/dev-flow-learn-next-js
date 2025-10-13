43814d33c8c5d4f36e1a7f82698431c6d0ce521e
sebaaismail <sebaa.ismail@gmail.com>
2025-10-13 13:15:11 +0100

    Assets and Metadata

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,8 @@ import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
@@ -11,6 +13,24 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  prettierConfig,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          semi: true,
+          singleQuote: false,
+          tabWidth: 2,
+          trailingComma: "es5",
+          endOfLine: "auto",
+        },
+      ],
+    },
+  },
   {
     ignores: [
       "node_modules/**",
