56b7c6341e53613eb5e743095a141f945d5ac544
sebaaismail <sebaa.ismail@gmail.com>
2025-11-11 21:36:02 +0100

    Implement Local search Bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,9 +4,53 @@ import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
 import Link from "next/link";
+import { de, ta, vi } from "zod/locales";
 
-const Home = async () => {
+const questions = [
+  {
+    _id: "1",
+    title: "How to learn React?",
+    description:
+      "I am new to web development and want to learn React. Any suggestions?",
+    tags: ["react", "javascript"],
+    author: { _id: "1", name: "John Doe" },
+    upvotes: 10,
+    answers: 2,
+    views: 150,
+    createdAt: new Date("2024-01-01"),
+  },
+  {
+    _id: "2",
+    title: "How to learn JavaScript?",
+    description:
+      "I am new to programming and want to learn JavaScript. Any suggestions?",
+    tags: ["javascript", "programming"],
+    author: { _id: "2", name: "Jane Smith" },
+    upvotes: 5,
+    answers: 1,
+    views: 100,
+    createdAt: new Date("2024-02-01"),
+  },
+];
+
+interface SearchParams {
+  searchParams: Promise<{ [key: string]: string }>;
+}
+
+const Home = async ({ searchParams }: SearchParams) => {
   const session = await auth();
+
+  const { query = "" } = await searchParams;
+
+  // const data = await axios.get("/api/questions", {
+  //   query: {
+  //     search: query ,
+  //   },
+  // });
+
+  const filteredQuestions = questions.filter((question) =>
+    question.title.toLowerCase().includes(query.toLowerCase())
+  );
 
   return (
     <>
@@ -29,10 +73,9 @@ const Home = async () => {
       </section>
       HomeFilter
       <div className="flex flex-col gap-6 w-full mt-10">
-        <p>QuestionCard 1</p>
-        <p>QuestionCard 2</p>
-        <p>QuestionCard 3</p>
-        <p>QuestionCard 4</p>
+        {filteredQuestions.map((question) => (
+          <h1 key={question._id}>{question.title}</h1>
+        ))}
       </div>
     </>
   );
--- components/search/LocalSearch.tsx
+++ components/search/LocalSearch.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Input } from "../ui/input";
-import { useSearchParams } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { formUrlQuery, removeKeysFromUrlQuery } from "@/lib/url";
 
 interface Props {
   route: string;
@@ -13,39 +14,54 @@ interface Props {
 }
 
 const LocalSearch = ({ route, imgSrc, placeholder, otherClasses }: Props) => {
+  const router = useRouter();
   const searchParams = useSearchParams();
   const query = searchParams.get("query") || "";
-  console.log("Current query param:", searchParams.toString());
+  const pathname = usePathname();
 
   const [searchQuery, setSearchQuery] = useState(query);
 
-  const handleSearch = (e: React.FormEvent) => {
-    e.preventDefault();
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (searchQuery) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "query",
+          value: searchQuery,
+        });
 
-    const newUrl = `${route}?query=${searchQuery}`;
-    window.location.href = newUrl;
-  };
+        router.push(newUrl, { scroll: false });
+      } else {
+        if (pathname === route) {
+          const newUrl = removeKeysFromUrlQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["query"],
+          });
+
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 300);
+  }, [searchQuery, router, searchParams]);
 
   return (
-    <form onSubmit={handleSearch} className="w-full">
-      <div
-        className={`flex background-light800_darkgradient min-h-14 grow px-4 items-center rounded-[10px] gap-4 ${otherClasses} `}
-      >
-        <Image
-          src={imgSrc}
-          alt="Search"
-          width={24}
-          height={24}
-          className="cursor-pointer"
-        />
-        <Input
-          className="paragraph-regular !bg-transparent no-focus placeholder text-dark400_light700 border-none shadow-none outline-none"
-          placeholder={placeholder}
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-      </div>
-    </form>
+    <div
+      className={`flex background-light800_darkgradient min-h-14 grow px-4 items-center rounded-[10px] gap-4 ${otherClasses} `}
+    >
+      <Image
+        src={imgSrc}
+        alt="Search"
+        width={24}
+        height={24}
+        className="cursor-pointer"
+      />
+      <Input
+        className="paragraph-regular !bg-transparent no-focus placeholder text-dark400_light700 border-none shadow-none outline-none"
+        placeholder={placeholder}
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+    </div>
   );
 };
 
